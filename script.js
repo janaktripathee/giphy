1641c909cf5ac026a2924a3441a702e5a125d88e
janak <janaktripathee@gmail.com>
2019-03-19 21:13:55 -0700

    api key updated

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
     $(this).addClass("active");
 
     var type = $(this).attr("data-type");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=27S1z4Jv5SsPymN8Cn2jf78TINSj17tI&limit=12";
 
     $.ajax({
       url: queryURL,
